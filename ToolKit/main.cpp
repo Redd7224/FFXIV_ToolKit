0cd63c7add4042d42e3a3011bbfd4e6150fe53a1
jonhall24 <hall.r.jonathan@gmail.com>
2020-03-15 02:46:34 -0400

    Small clean up on main

--- ToolKit/main.cpp
+++ ToolKit/main.cpp
@@ -6,15 +6,13 @@ using namespace std;
 int main()
 {
 	FileProcessor* fileProcessor = new FileProcessor();
-	
+
 	string filePath = "..\\..\\TestFiles\\CraftingItems.txt";
 	fileProcessor->SetFile(filePath);
 	fileProcessor->ProcessFile();
 
 	for (int i = 0; i < fileProcessor->GetGear().size(); i++)
-	{
 		cout << fileProcessor->GetGearAtIndex(i) << endl;
-	}
 
 	system("pause");
 	return 0;
